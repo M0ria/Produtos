a87f11bbc9141e16f32effe43abaa05d09a88b2a
Maria <rita.castro@FZDTD6209.sangatiberga.com.br>
2024-11-15 15:41:41 -0300

    alterações nas rotas e criando o controller

--- src/router/product.routes.ts
+++ src/router/product.routes.ts
@@ -1,66 +1,84 @@
 import { Request, Response, Router } from "express";
-import { products } from "../data/product";
+import {
+    getAllProducts,
+    getProductsByCategory,
+    createProduct,
+    deleteProductById
+} from "../controllers/productController";
 
-const router = Router()
-router
-    .get("/products", (req: Request, res: Response) => {
-        res.status(200).json(products)
-    })
-    .get("/products/category/:category", (req: Request, res: Response) => {
+const router = Router();
 
-        // CATEGORY É O PARÂMETRO QUE O USUÁRIO ME ENVIA NA REQUISIÇÃO.
-        const { category } = req.params
+router.get("/products", getAllProducts);
+router.get("/products/category/:category", getProductsByCategory);
+router.post("/products", createProduct);
+router.delete("/products/id/:id", deleteProductById);
 
-        // MINHA NEW LIST É A LISTA DE ELEMENTOS BASEADO NA CATEGORIA QUE O USUÁRIO REQUISITOU.
-        const newList = products.filter((product) => {
-            return product.category === category
-        })
-        res.status(200).json(newList)
-    })
-    .post("/products", (req: Request, res: Response) => {
-        //CORPO QUE RECEBO COM AS INFORMAÇÕES DO MEU PRODUTO DO CLIENT
-        const { name, description, specifications, image, category } = req.body
+export { router };
 
-        //FORMA OLHANDO PARA A CHAVE DO OBJETO
-        const hasInvalidKeys = !name || !description || !category
-        const hasEmptyKeys = name.trim() === "" || description.trim() === "" || category.trim() === ""
+// import { Request, Response, Router } from "express";
+// import { products } from "../data/product";
 
-        if (hasInvalidKeys || hasEmptyKeys) {
-            res.status(400).send('Chave name, descrição e categoria são obrigatórias.')
-        }
+// const router = Router()
+// router
+//     .get("/products", (req: Request, res: Response) => {
+//         res.status(200).json(products)
+//     })
+//     .get("/products/category/:category", (req: Request, res: Response) => {
 
-        const newProduct = {
-            id: Date.now(),
-            name,
-            description,
-            category,
-            specifications: specifications || {},
-            image: image || "",
-        }
+//         // CATEGORY É O PARÂMETRO QUE O USUÁRIO ME ENVIA NA REQUISIÇÃO.
+//         const { category } = req.params
 
-        products.push(newProduct)
+//         // MINHA NEW LIST É A LISTA DE ELEMENTOS BASEADO NA CATEGORIA QUE O USUÁRIO REQUISITOU.
+//         const newList = products.filter((product) => {
+//             return product.category === category
+//         })
+//         res.status(200).json(newList)
+//     })
+//     .post("/products", (req: Request, res: Response) => {
+//         //CORPO QUE RECEBO COM AS INFORMAÇÕES DO MEU PRODUTO DO CLIENT
+//         const { name, description, specifications, image, category } = req.body
 
-        res.status(201).json(newProduct)
-    })
-    .delete("/products/id/:id", (req: Request, res: Response) => {
-        const { id } = req.params
-        const deleteIndex = products.findIndex((product) => {
-            // Maneiras de converter string numerica em Number
-            // -> id: '1' => +id
-            // parseInt(id)
-            // Number(id)
-            return product.id === Number(id)
-        })
-        //verefica de o index(posição) é igual a -1, que não existe, então por isso dá erro se tiver nenhum indice
-        if (deleteIndex === -1) {
-            res.status(401).json()
-        } 
-        //aqui cria a const de deletar o produto, que vai ser tudo da array de products e com o splice ele pega o elemento e a posição que ele se encontra
-        const deleteProduct = products.splice(deleteIndex, 1);
+//         //FORMA OLHANDO PARA A CHAVE DO OBJETO
+//         const hasInvalidKeys = !name || !description || !category
+//         const hasEmptyKeys = name.trim() === "" || description.trim() === "" || category.trim() === ""
 
-        res.sendStatus(204)
-    })
+//         if (hasInvalidKeys || hasEmptyKeys) {
+//             res.status(400).send('Chave name, descrição e categoria são obrigatórias.')
+//         }
+
+//         const newProduct = {
+//             id: Date.now(),
+//             name,
+//             description,
+//             category,
+//             specifications: specifications || {},
+//             image: image || "",
+//         }
+
+//         products.push(newProduct)
+
+//         res.status(201).json(newProduct)
+//     })
+//     .delete("/products/id/:id", (req: Request, res: Response) => {
+//         const { id } = req.params
+//         console.log(id);
+//         const deleteIndex = products.findIndex((product) => {
+//             // Maneiras de converter string numerica em Number
+//             // -> id: '1' => +id
+//             // parseInt(id)
+//             // Number(id)
+//             return product.id === Number(id)
+//         })
+//         //verefica de o index(posição) é igual a -1, que não existe, então por isso dá erro se tiver nenhum indice
+//         if (deleteIndex === -1) {
+//             res.status(401).json()
+//         } 
+//         //aqui cria a const de deletar o produto, que vai ser tudo da array de products e com o splice ele pega o elemento e a posição que ele se encontra
+//         const deleteProduct = products.splice(deleteIndex, 1);
+
+//         res.sendStatus(204)
+//     })
 
 
 
-export { router }
+// export { router }
